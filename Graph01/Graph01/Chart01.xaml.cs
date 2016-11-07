bbfc8fa0b5a38822b2faaab2d71e6ed10f4479fc
KienPham-PC\KienPham <kienph@beetsoft.com.vn>
2016-11-07 14:09:53 +0700

    update

---
+++ Graph01/Graph01/Chart01.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Graph01
+{
+    /// <summary>
+    /// Interaction logic for Chart01.xaml
+    /// </summary>
+    public partial class Chart01 : Window
+    {
+        public Chart01()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Country
+    {
+        public Country(string Name, long Population)
+        {
+            this.Name = Name;
+            this.Population = Population;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public long Population
+        {
+            get;
+            set;
+        }
+    }
+
+    public class CountryCollection : Collection<Country>
+    {
+        public CountryCollection()
+        {
+            Add(new Country("China", 1336718015));
+            Add(new Country("India", 1189172906));
+            Add(new Country("United States", 313232044));
+            Add(new Country("Indonesia", 245613043));
+            Add(new Country("Brazil", 203429773));
+        }
+    }
+}
---
+++ RealTime/YTGraphWPFTester/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace YTGraphWPFTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime timeStarted;
+        double yValue = 2;
+        double dVariance = 0;
+        Random rand;
+        DispatcherTimer timer = new DispatcherTimer();
+        List<int> list = new List<int>(new int[] { 0, 0, 0, -1, -1, -1, -3, -3, -3, -5, -5, -5, 1, 1, 1, 3, 3, 3, 4, 4, 4, -10, -10, -10, -20, -20, -20, -30, -30, -30 });
+        int i;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            graphUC.CheckAndAddSeriesToGraph("roto", "rpm");
+            dVariance = yValue;
+            rand = new Random();
+            timeStarted = DateTime.Now;
+            timer.Interval = TimeSpan.FromMilliseconds(2000);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        public int Value()
+        {
+            //Random r = new Random();
+            //int index = r.Next(0, list.Count - 1);
+            if(i <= (list.Count -1))
+            {
+                return list.ElementAt(i);
+            }
+            else 
+            {
+                i = 0;
+                return list.ElementAt(i);
+            }
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            double dValueX = (double)(DateTime.Now.Subtract(timeStarted).TotalSeconds);
+
+            graphUC.AddPointToLine("roto", yValue + (Value() - dVariance), dValueX);
+
+            i++;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            graphUC.Width = e.NewSize.Width - 40;
+            graphUC.Height = e.NewSize.Height - 20;
+        }
+    }
+}
